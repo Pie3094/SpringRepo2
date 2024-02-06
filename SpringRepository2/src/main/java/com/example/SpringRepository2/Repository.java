7129ccbe91c58319c8c14d60d4fd09a6e7fa6069
Pie3094 <145662095+Pie3094@users.noreply.github.com>
2024-02-06 15:12:32 +0100

    Delete SpringRepository2 directory

--- SpringRepository2/src/main/java/com/example/SpringRepository2/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringRepository2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.Description;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "repo-languages", collectionResourceDescription = @Description("Descrizione della classe"))
-public interface Repository extends JpaRepository<ProgrammingLanguage, Long> {
-}
